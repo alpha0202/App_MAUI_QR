78c03742226de10683cf2265de49c4d30f64543c
edwin harley martinez gomez <alpha0202@gmail.com>
2023-11-09 18:27:20 -0500

    Generando texto personalizado para QR

---
+++ App_MAUI_QR/Models/GenerarQRModel.cs
@@ -0,0 +1,29 @@
+﻿using App_MAUI_QR.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_MAUI_QR.Models
+{
+    public class GenerarQRModel :BaseBinding
+    {
+        private string _valorQR;
+
+        public string valorQR 
+        {
+            get
+            {
+                return _valorQR;
+            }
+            set 
+            { 
+                SetValue(ref _valorQR, value);
+            
+            } 
+        }
+                
+               
+    }
+}
--- App_MAUI_QR/Page/GenerarQR.xaml.cs
+++ App_MAUI_QR/Page/GenerarQR.xaml.cs
@@ -1,10 +1,19 @@
+using App_MAUI_QR.Models;
+
 namespace App_MAUI_QR.Page;
 
 public partial class GenerarQR : ContentPage
 {
-	public GenerarQR()
+
+    public GenerarQRModel oGenerarQRModel { get; set; }
+    public string TextoValor { get; set; }
+   
+
+    public GenerarQR()
 	{
 		InitializeComponent();
+        oGenerarQRModel = new GenerarQRModel();
+        BindingContext = this;
 	}
 
     private void btnRegresar_Clicked(object sender, EventArgs e)
@@ -12,5 +21,16 @@ public partial class GenerarQR : ContentPage
         App.Current.MainPage = new PrincipalPage();
     }
 
-    
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if(!String.IsNullOrEmpty(TextoValor))
+        {
+            oGenerarQRModel.valorQR = TextoValor;
+
+        }
+        else
+        {
+            throw new Exception("Debe ingresar un valor texto");
+        }
+    }
 }
